805cf16a35d76b3d670d8d684ebcb18bdfe76043
Ardian Agoes <ardianagoes83@gmail.com>
2026-06-03 10:01:12 -0400

    updated walking and checking person position for bridge crossing

--- main.py
+++ main.py
@@ -277,21 +277,22 @@ while True:
         s.model.axis = spring_vec
         
     for person in person_list:
-        person.model.pos += person.vel * dt if (p.model.pos.x < BRIDGE_LEN/2) else 0
-
-        on_bridge = abs(p.model.pos.x) < BRIDGE_LEN/2
-        if on_bridge:
-            for p in plank_list:
-                if (p.model.pos.x - p.model.size.x/2) <= person.model.pos.x <= (p.model.pos.x + p.model.size.x/2):
-                    p.net_force += vec(0, -person.mass * 9.8, 0)
+            if person.model.pos.x < (BRIDGE_LEN/2 + WALL_WIDTH*PPL_NUM):
+                person.model.pos += person.vel * dt         
+            on_bridge = abs(person.model.pos.x) < BRIDGE_LEN/2
+            
+            if on_bridge:
+                for p in plank_list:
+                    if (p.model.pos.x - p.model.size.x/2) <= person.model.pos.x <= (p.model.pos.x + p.model.size.x/2):
+                        p.net_force += vec(0, -person.mass * 9.8, 0)
+                        
+                        dx = person.model.pos.x - p.model.pos.x
+                        surface_y = p.model.pos.y + dx * tan(p.angle) + (p.model.size.y/2)/cos(p.angle)
+                        person.model.pos.y = surface_y + person.model.size.y/2
+                        break
                     
-                    dx = person.model.pos.x - p.model.pos.x
-                    surface_y = p.model.pos.y + dx * tan(p.angle) + (p.model.size.y/2)/cos(p.angle)
-                    person.model.pos.y = surface_y + person.model.size.y/2
-                    break
-                
-        if not on_bridge: 
-            person.model.pos.y = PPL_DIM.y/2
+            if not on_bridge: 
+                person.model.pos.y = PPL_DIM.y/2
 
     for p in plank_list:
         if not p.anchored:
