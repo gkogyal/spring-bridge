507eb8922eeddaaabf02812f34328c555a6a18c7
gkogyal <gkogyal60@stuy.edu>
2026-06-05 09:53:08 -0400

    input widgets, bridge breaking

--- main.py
+++ main.py
@@ -16,7 +16,13 @@ BRZ_MATERIAL = 2 # bronze
 
 PLA_WIDTH = 10
 
-WALL_WIDTH = 5
+WALL_WIDTH = 10
+
+BREAK_THRESHOLDS = [
+    [18.0, 22.0, 12.0],   # steel
+    [12.0, 15.0,  8.0],   # aluminum
+    [10.0, 13.0,  7.0],   # bronze
+]
 
 #########################
 # INPUTS
@@ -28,29 +34,114 @@ PPL_SPEED = 5 # m/s
 
 PLA_NUM = 10
 
-SPR_K = 25 # N/m
+SPR_K = 250 # N/m
 SPR_NUM = 1 # in parallel between each
 SPR_TYPE = 0 # defaults to torsion type
 SPR_MAT = 0 # defaults to aluminum
 SPR_B = 2 # dampening constant
 
-extra_visuals = True
+extra_visuals = False
+
+#########################
+# INPUT WIDGETS
+#########################
+
+def widget():
+    scene.select()
+    
+    scene.append_to_caption("<b>Parameters</b>\n\n")
+    
+    scene.append_to_caption("<b>--- People ---</b>\n")
+    
+    scene.append_to_caption("# People: ")
+    def set_ppl_num(s):
+        global PPL_NUM
+        nonlocal ppl_num_label
+        PPL_NUM = int(s.number)
+        ppl_num_label.text = f'{int(s.number)}'
+    slider(min=1, max=10, value=PPL_NUM, step=1, length=180, bind=set_ppl_num)
+    ppl_num_label = wtext(text=f'{PPL_NUM}')
+    
+    scene.append_to_caption("\nMass (kg): ")
+    def set_ppl_mass(s):
+        global PPL_MASS
+        nonlocal ppl_mass_label
+        PPL_MASS = s.number
+        ppl_mass_label.text = f'{s.number:.1f}'
+    slider(min=1, max=200, value=PPL_MASS, step=1, length=180, bind=set_ppl_mass)
+    ppl_mass_label = wtext(text=f'{PPL_MASS:.1f}')
+    
+    scene.append_to_caption("\nSpeed (m/s): ")
+    def set_ppl_speed(s):
+        global PPL_SPEED
+        PPL_SPEED = s.number
+        ppl_speed_label.text = f'{s.number:.1f}'
+    slider(min=1, max=20, value=PPL_SPEED, step=0.5, length=180, bind=set_ppl_speed)
+    ppl_speed_label = wtext(text=f'{PPL_SPEED:.1f}')
+    
+    scene.append_to_caption("\n\n<b>--- Planks ---</b>\n")
+    
+    scene.append_to_caption("# Planks: ")
+    def set_pla_num(s):
+        global PLA_NUM
+        PLA_NUM = int(s.number)
+        pla_num_label.text = f'{int(s.number)}'
+    slider(min=4, max=20, value=PLA_NUM, step=1, length=180, bind=set_pla_num)
+    pla_num_label = wtext(text=f'{PLA_NUM}')
+    
+    scene.append_to_caption("\n\n<b>--- Springs ---</b>\n")
+    
+    scene.append_to_caption("k (N/m): ")
+    def set_spr_k(s):
+        global SPR_K
+        SPR_K = s.number
+        spr_k_label.text = f'{s.number:.0f}'
+    slider(min=100, max=500, value=SPR_K, step=10, length=180, bind=set_spr_k)
+    spr_k_label = wtext(text=f'{SPR_K:.0f}')
+    
+    scene.append_to_caption("\nParallel #: ")
+    def set_spr_num(s):
+        global SPR_NUM
+        SPR_NUM = int(s.number)
+        spr_num_label.text = f'{int(s.number)}'
+    slider(min=1, max=5, value=SPR_NUM, step=1, length=180, bind=set_spr_num)
+    spr_num_label = wtext(text=f'{SPR_NUM}')
+    
+    scene.append_to_caption("\nDamping b: ")
+    def set_spr_b(s):
+        global SPR_B
+        SPR_B = s.number
+        spr_b_label.text = f'{s.number:.1f}'
+    slider(min=0, max=20, value=SPR_B, step=1, length=180, bind=set_spr_b)
+    spr_b_label = wtext(text=f'{SPR_B:.1f}')
+    
+    scene.append_to_caption("\n\nType: ")
+    def set_spr_type(m):
+        global SPR_TYPE
+        SPR_TYPE = m.index
+    menu(choices=["Torsion", "Tension", "Compression"], index=SPR_TYPE, bind=set_spr_type)
+    
+    scene.append_to_caption("  Mat: ")
+    def set_spr_mat(m):
+        global SPR_MAT
+        SPR_MAT = m.index
+    menu(choices=["Steel", "Aluminum", "Bronze"], index=SPR_MAT, bind=set_spr_mat)
+    
+    scene.append_to_caption("\n\n")
+
 
 #########################
 # SCENE SETUP
 #########################
 
-scene.userzoom = False
-scene.resizable = False
+#scene.userzoom = False
+#scene.resizable = False
+scene.length = 10
 scene.width = 1000
 scene.background = color.white
 scene.title = "<b>PHYSICS FINAL PROJECT: Spring Bridge</b>"
 
-scene2 = canvas()
 scene.select()
-scene.visible = True
-scene2.visible = True
-
 #########################
 # WORLD
 #########################
@@ -67,22 +158,27 @@ FLOOR = box(pos=vec(0, -50, 0), size=vec(BRIDGE_LEN + 4*WALL_WIDTH*PPL_NUM, 10, 
 
 person_list = []
 
-PPL_DIM = vec(5,10,5) # dimension of person
+PPL_DIM = vec(10,20,10) # dimension of person
 class Person:
     def __init__(self, start_pos, mass):
-        self.pos = start_pos
         self.mass = mass
         self.vel = vec(0,0,0) # vec(PPL_SPEED,0,0)
         
-        self.model = ellipsoid(
+        self.model = box(
             texture = textures.wood,
-            pos = self.pos,
+            pos = start_pos,
             size = PPL_DIM,
             color = vec(0,0,0)
         )
         
 def init_people():
     global person_list
+    
+    # temp
+    for ppl in person_list:
+        ppl.model.visible = False
+    person_list = []
+
     
     first_init = len(person_list)==0
     
@@ -92,7 +188,6 @@ def init_people():
             ppl = Person(start_pos=init_pos, mass = PPL_MASS)
             person_list.append(ppl)
         else:
-            person_list[i].pos = init_pos
             person_list[i].model.pos = init_pos
             person_list[i].model.visible = True
 
@@ -104,7 +199,6 @@ plank_list = []
 
 class Plank:
     def __init__(self, start_pos, mass, length=PLA_WIDTH, height = 2.0, width=50.0):
-        self.pos = start_pos
         self.mass = mass
         self.velocity = vec(0, 0, 0)
         self.net_force = vec(0, 0, 0)
@@ -113,13 +207,19 @@ class Plank:
 
         self.model = box(
             texture=textures.wood,
-            pos=self.pos,
+            pos=start_pos,
             size=vec(length, height, width),
             color=vec(0.5, 0.25, 0)
         )
 
 def init_planks():
     global plank_list
+    
+    # temp
+    for pla in plank_list:
+        pla.model.visible = False
+    plank_list = []
+
     
     first_init = len(plank_list)==0
     
@@ -136,7 +236,6 @@ def init_planks():
             plank_list.append(pla)
         else:
             plank_list[i].model.pos = init_pos
-            plank_list[i].pos = init_pos
             plank_list[i].velocity = vec(0,0,0)
             plank_list[i].acceleration = vec(0,0,0)
 
@@ -153,6 +252,8 @@ class Spring:
         self.k = k
         self.plankL = plankL
         self.plankR = plankR
+        self.broken = False
+        self.break_threshold = BREAK_THRESHOLDS[SPR_MAT][SPR_TYPE]
         
         self.rest_length = mag(plankL.model.pos - plankR.model.pos)
         
@@ -164,12 +265,16 @@ class Spring:
 
 def init_springs():
     global spring_list
-    
+
+    for spr in spring_list:
+        spr.model.visible = False
+    spring_list = []
+
     first_init = len(spring_list)==0
     
     for i in range(PLA_NUM-1):
         if first_init:
-            s = Spring(k=200, plankL = plank_list[i], plankR = plank_list[i+1])
+            s = Spring(k=SPR_K*SPR_NUM, plankL = plank_list[i], plankR = plank_list[i+1])
             spring_list.append(s)
         else:
             spring_list[i].angle = 0
@@ -224,7 +329,7 @@ def advance():
         person.vel = vec(PPL_SPEED,0,0)
     
     BUTTON_PPL.delete()
-    BUTTON_PPL = button(bind=cease, text = 'CEASE', background=color.red)
+    BUTTON_PPL = button(bind=cease, text = 'CEASE\n', background=color.red)
         
 def cease():
     global person_list
@@ -234,13 +339,19 @@ def cease():
         person.vel = vec(0,0,0)
         
     BUTTON_PPL.delete()
-    BUTTON_PPL = button(bind=advance, text = 'ADVANCE', background=color.green)
+    BUTTON_PPL = button(bind=advance, text = 'ADVANCE\n', background=color.green)
 
 def start():
-    global BUTTON_MAIN
-    global BUTTON_PPL
-    global go
-    global t
+    global BUTTON_MAIN, BUTTON_PPL, go , t
+    global BRIDGE_LEN, WALL_L, WALL_R, FLOOR
+
+    BRIDGE_LEN = PLA_WIDTH * PLA_NUM
+    WALL_L.pos = vec(-BRIDGE_LEN/2 - WALL_WIDTH*PPL_NUM, -25, 0)
+    WALL_L.size = vec(WALL_WIDTH*2*PPL_NUM, 50, 100)
+    WALL_R.pos = vec(BRIDGE_LEN/2 + WALL_WIDTH*PPL_NUM, -25, 0)
+    WALL_R.size = vec(WALL_WIDTH*2*PPL_NUM, 50, 100)
+    FLOOR.size = vec(BRIDGE_LEN + 4*WALL_WIDTH*PPL_NUM, 10, 100)
+
     init_people()
     init_planks()
     init_springs()
@@ -251,8 +362,8 @@ def start():
     pe_curve.data = []
     te_curve.data = []
     BUTTON_MAIN.delete()
-    BUTTON_MAIN = button(bind=stop, text='RESET', background=color.red)
-    BUTTON_PPL = button(bind=advance, text = 'ADVANCE', background=color.green)
+    BUTTON_MAIN = button(bind=stop, text='RESET\n', background=color.red)
+    BUTTON_PPL = button(bind=advance, text = 'ADVANCE\n', background=color.green)
     
     # ADD: start() updates certain buttons to be available
 
@@ -275,12 +386,13 @@ def stop():
 
     go = False
     BUTTON_MAIN.delete()
-    BUTTON_MAIN = button(bind=start, text='START', background=color.green)
+    BUTTON_MAIN = button(bind=start, text='START\n', background=color.green)
     BUTTON_PPL.delete()
     
     # ADD: stop() updates certain buttons to be available
+widget()
 
-BUTTON_MAIN = button(bind=start, text='START', background=color.green)
+BUTTON_MAIN = button(bind=start, text='START\n', background=color.green)
 
 #########################
 # MAIN
@@ -304,6 +416,11 @@ while True:
         p.net_force = vec(0, 0, 0)
     
     for s in spring_list:
+        if s.broken:
+            s.model.visible = False
+            continue
+
+        
         spring_vec = s.plankR.model.pos - s.plankL.model.pos
         current_length = mag(spring_vec)
         
@@ -313,7 +430,16 @@ while True:
             spring_dir = vec(0,0,0)
 
         stretch = current_length - s.rest_length
-        tension_mag = s.k * stretch
+        
+        if stretch > s.break_threshold:
+            s.broken = True
+            s.model.color = color.red
+            s.model.visible = False
+            continue
+        
+        
+        
+        tension_mag = s.k* SPR_NUM   * stretch
 
         s.plankL.net_force += tension_mag * spring_dir
         s.plankR.net_force += -tension_mag * spring_dir
@@ -321,13 +447,20 @@ while True:
         s.model.pos = s.plankL.model.pos
         s.model.axis = spring_vec
         
+        stress_ratio = min(stretch / s.break_threshold, 1.0)
+        if stress_ratio < 0.5:
+            s.model.color = vec(stress_ratio * 2, 1, 0) # GtY
+        else:
+            s.model.color = vec(1, 1 - (stress_ratio - 0.5) * 2, 0) # YtR
+
+        
     for person in person_list:
                 
         # check stopped here
             
         person.model.pos += person.vel * dt
         
-        on_bridge = abs(p.model.pos.x) < BRIDGE_LEN/2
+        on_bridge = abs(person.model.pos.x) < BRIDGE_LEN/2
         if on_bridge:            
             for p in plank_list:
                 if (p.model.pos.x - p.model.size.x/2) <= person.model.pos.x <= (p.model.pos.x + p.model.size.x/2):
@@ -372,11 +505,8 @@ while True:
         p.model.rotate(angle=theta - p.angle, axis=vec(0, 0, 1), origin=pivot)
         p.angle = theta
 
-    # ==========================================
     # ENERGY CALCULATIONS & GRAPHING
-    # ==========================================
 
-    # Energy calculations
     sys_ke = 0
     sys_pe_grav = 0
     sys_pe_spring = 0
@@ -398,6 +528,6 @@ while True:
     te_curve.plot(t, sys_energy_total)
 
     # Displacement graphing
-    center_index = PLA_NUM // 2
+    center_index = len(plank_list) // 2
     center_y = plank_list[center_index].model.pos.y
     disp_curve.plot(t, center_y)
